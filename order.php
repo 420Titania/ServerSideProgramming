9a3bfdfbbaca9c429a2185d4f4103aaeb80644d5
420Titania <kashira.naratama@student.sgu.ac.id>
2024-06-03 10:51:15 +0700

    Made changes around the order page. Will fix it.

--- order.php
+++ order.php
@@ -11,18 +11,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pathway+Gothic+One&display=swap" rel="stylesheet">
-    <style>
-       .tabhead {
-            font-size: 48px;
-        }
-        td {
-            font-size: 28px;
-        }
-    </style>
 </head>
     <body>
         <div class="topnav" id="topnav">
-                <a href="points.html">Points</a>
+                <a href="points.php">Points</a>
                 <a href="order.php" class="active">Order</a>
                 <a href="index.php#popular" id="section3">Popular</a>
                 <a href="index.php#about" id="section2">About</a>
@@ -33,53 +25,93 @@
         <!--Whole page hasn't even started yet. Will work on it soon. And probably redesign it overall.-->
         <div class="wholepage" id="order">
 
+
             <div class="title">
-            <p>Choose from a wide variety of [REDACTED]!</p>
+                <head2>Order Now</head2>
             </div>
-            <div class="menu">
+            <div class="menu-page">
+                <div class="menu-container">
+                    <div class="menu">
+                        <?php
+                        require_once('scripts/config.php');
+                        
+                        $sql1 = "SELECT * FROM menu WHERE type='coffee'";
+                        $coffee = mysqli_query($conn, $sql1);
+                        
+                        if (mysqli_num_rows($coffee) > 0) {
+                            echo '<table>
+                                    <tr>
+                                        <td colspan="2" class="menu-title">Coffees</td>
+                                    </tr>';
+                            while($row = mysqli_fetch_assoc($coffee)) {
+                                echo '<tr>
+                                        <td>'.$row['name'].'</td>
+                                        <td>Rp. '.$row['price'].'</td>
+                                    </tr>';
+                            }
+                            echo '</table>';
+                        } else {
+                            echo 'No data found';
+                        }
+                        ?>
+                    </div>
+                </div>
 
-            <?php
-                require_once('scripts/config.php');
+                <div class="menu-container">
+                    <div class="menu">
+                        <?php
+                        require_once('scripts/config.php');
+                        
+                        $sql2 = "SELECT * FROM menu WHERE type='noncoffee'";
+                        $noncoffee = mysqli_query($conn, $sql2);
+                        
+                        if (mysqli_num_rows($noncoffee) > 0) {
+                            echo '<table>
+                                    <tr>
+                                        <td colspan="2" class="menu-title">Non-Coffee</td>
+                                    </tr>';
+                            while($row = mysqli_fetch_assoc($noncoffee)) {
+                                echo '<tr>
+                                        <td>'.$row['name'].'</td>
+                                        <td>Rp. '.$row['price'].'</td>
+                                    </tr>';
+                            }
+                            echo '</table>';
+                        } else {
+                            echo 'No data found';
+                        }
+                        ?>
+                    </div>
+                </div>
 
-                $sql1 = "SELECT * FROM menu WHERE type='coffee'";
-                $coffee = mysqli_query($conn, $sql1);
-                
-                if (mysqli_num_rows($coffee) > 0) {
-                    echo '<table style="width:40%">
-                            <tr>
-                                <td colspan="3" class="tabhead">Coffees</td>
-                            </tr>';
-                    while($row = mysqli_fetch_assoc($coffee)) {
-                        echo '<tr>
-                                <td>'.$row['name'].'</td>
-                                <td>Rp. '.$row['price'].'</td>
-                            </tr>';
-                    }
-                    echo '</table>';
-                } else {
-                    echo 'No data found';
-                }
-
-                $conn->close();
-            ?>
-
-            <!-- <table style="width:80%">
-                <tr>
-                    <td>one</td>
-                    <td>one</td>
-                    <td>one</td>
-                </tr>
-                <tr>
-                    <td>two</td>
-                    <td>two</td>
-                    <td>two</td>
-                </tr>
-                <tr>
-                    <td colspan="3">three</td>
-                </tr>
-            </table> -->
+                <div class="menu-container">
+                    <div class="menu">
+                        <?php
+                        require_once('scripts/config.php');
+                        
+                        $sql3 = "SELECT * FROM menu WHERE type='Food'";
+                        $food = mysqli_query($conn, $sql3);
+                        
+                        if (mysqli_num_rows($food) > 0) {
+                            echo '<table>
+                                    <tr>
+                                        <td colspan="2" class="menu-title">Pastries</td>
+                                    </tr>';
+                            while($row = mysqli_fetch_assoc($food)) {
+                                echo '<tr>
+                                        <td>'.$row['name'].'</td>
+                                        <td>Rp. '.$row['price'].'</td>
+                                    </tr>';
+                            }
+                            echo '</table>';
+                        } else {
+                            echo 'No data found';
+                        }
+                        
+                        $conn->close();
+                        ?>
+                    </div>
+                </div>
             </div>
-        </div>
-
     </body>
 </html>
